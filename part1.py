ae96819d5b8303554199748a614bd7167c28665a
rivques <38469076+rivques@users.noreply.github.com>
2025-01-30 16:59:12 -0500

    work! pre-reset looks good, post is broken

--- part1.py
+++ part1.py
@@ -1,28 +1,54 @@
 from utils.drone_manager import DroneManager, DroneType
 from utils.basic_actions import SequentialAction, TakeoffAction, GoToAction, LandAction, ErrorHandlingStrategy, WaitAction, ReadColorAndSetLEDAction, FastTakeoffAction
 import logging
+import utils.field_locations as fl
 logging.basicConfig(level=logging.DEBUG)
 if __name__ == "__main__":
     drone_manager = DroneManager(drone_type=DroneType.REAL)
 
     SequentialAction(drone_manager, [
-        ReadColorAndSetLEDAction(),
-        FastTakeoffAction(1),
-        GoToAction(0, 0, 1, "TakeoffSettle", 4),
+        ReadColorAndSetLEDAction(), # +15 (15 total)
+        FastTakeoffAction(1), # +10 (25 total)
+        GoToAction(0, 0, 1, "TakeoffSettle", 3),
+        # start of fig8 #1
         GoToAction(1.4, None, None, "GoThruArch", 3),
         GoToAction(None, None, 1.7, "RaiseForOver", 3),
         GoToAction(2.4, None, None, "GoOverBlue", 3),
         GoToAction(None, None, 1.2, "LowerAroundBlue", 3),
         GoToAction(1.2, None, None, "GoToRed", 3),
-        GoToAction(None, None, 1.8, "RaiseForOver", 3),
-        GoToAction(0, None, None, "GoOverRed", 3),
-        GoToAction(0, 0, 1, "TakeoffSettle", 4),
+        GoToAction(None, None, 1.9, "RaiseForOver", 3),
+        GoToAction(0, None, None, "GoOverRed", 3), # +40 (65 total)
+        GoToAction(0, 0, 1.2, "DescendForArch", 4),
+        # start of fig8 #2
         GoToAction(1.4, None, None, "GoThruArch", 3),
         GoToAction(None, None, 1.8, "RaiseForOver", 3),
         GoToAction(2.4, None, None, "GoOverBlue", 3),
         GoToAction(None, None, 1.2, "LowerAroundBlue", 3),
-        GoToAction(1.2, None, None, "GoToRed", 3),
-        GoToAction(None, None, 1.8, "RaiseForOver", 3),
+        GoToAction(1.4, None, None, "GoToRed", 3), # +40 (105 total)
+        GoToAction(None, None, 1.9, "RaiseForOver", 3),
         GoToAction(0, None, None, "GoOverRed", 3),
+        # start of arch farming
+        GoToAction(0, 0, 1.2, "DescendForArch", 4),
+        GoToAction(2.4, None, None, "GoThruArches", 3), # +10 (115 total)
+        GoToAction(0, 0.2, None, "GoBack", 3),
+        GoToAction(2.4, None, None, "GoThruArchesAgain", 3), # +10 (125 total)
         LandAction()
+    ], ErrorHandlingStrategy.LAND).run_sequence()
+
+    input("Press ENTER to start Segment 2")
+
+    SequentialAction(drone_manager, [
+        ReadColorAndSetLEDAction(), # +15 (140 total)
+        FastTakeoffAction(fl.yellow_keyhole[2], timeout=0), # +10 (150 total)
+        GoToAction(fl.mat_2[0], fl.mat_2[1], None, "ReestablishTakeoff", timeout=0), # because the drone wants to go to 0,0 on TO
+        WaitAction(3),
+        GoToAction(fl.yellow_keyhole[0], fl.yellow_keyhole[1]+0.2, None, "GoThruYellowKeyhole", 3), # +15 (165 total)
+        GoToAction(None, fl.yellow_keyhole[1] - 0.2, None, "GoBack", 3),
+        GoToAction(None, fl.green_keyhole[1], None, "GoThruYellowKeyholeAgain", 3), # +15 (180 total)
+        GoToAction(None, None, fl.green_keyhole[2], "LowerForGreenKeyhole", 3),
+        GoToAction(fl.green_keyhole[0]+0.2, None, None, "GoThruGreenKeyhole", 3), # +15 (195 total)
+        GoToAction(fl.green_keyhole[0]-0.1, fl.green_keyhole[1], None, "GoBack", 3),
+        GoToAction(fl.green_keyhole[0]+0.2, None, None, "GoThruGreenKeyholeAgain", 3), # +15 (210 total)
+        GoToAction(fl.large_landing_cube[0], fl.large_landing_cube[1], None, "GoToLandingCube", 3),
+        LandAction() # +25 (235 total)
     ], ErrorHandlingStrategy.LAND).run_sequence()
--- utils/field_locations.py
+++ utils/field_locations.py
@@ -15,14 +15,16 @@ arch_inner_height = in_to_m(63)
 landing_pad_dia = in_to_m(15)
 
 mat_1 = np.array([0, 0, 0])
-mat_2 = np.array([prog_mat_size[0]+prog_mat_size[1]/2-in_to_m(6), prog_mat_size[1]/2+in_to_m(25)+color_mat_size/2, 0])
+mat_2 = np.array([color_mat_size/2+prog_mat_size[0]+prog_mat_size[1]/2, in_to_m(3), 0])
 
-red_arch = np.array([color_mat_size/2, 0, arch_inner_height/2])
-blue_arch = np.array([prog_mat_size[0]+prog_mat_size[1]/2, prog_mat_size[1]/2+in_to_m(54-24), arch_inner_height/2])
+reset_offset = -mat_2
 
-yellow_keyhole = np.array([in_to_m(72)+color_mat_size/2, -(-prog_mat_size[1]/2+in_to_m(24)), in_to_m(38)+keyhole_outer_dia/2])
-green_keyhole = np.array([prog_mat_size[0]+prog_mat_size[1]-in_to_m(45), prog_mat_size[1]/2, in_to_m(46)+keyhole_outer_dia/2])
+red_arch = np.array([color_mat_size/2+in_to_m(24), 0, arch_inner_height/2])
+blue_arch = np.array([color_mat_size/2+in_to_m(72), 0, arch_inner_height/2])
 
-blue_landing_pad = np.array([landing_pad_dia/2+color_mat_size/2, in_to_m(3), 0])
-red_landing_pad = np.array([-(landing_pad_dia/2+color_mat_size/2), in_to_m(3), 0])
-green_landing_pad = np.array([0, -color_mat_size+0.2, 0])
+yellow_keyhole = np.array([color_mat_size/2+prog_mat_size[0]+in_to_m(18), in_to_m(57), in_to_m(54)+keyhole_outer_dia/2])
+green_keyhole = np.array([color_mat_size/2+prog_mat_size[0], in_to_m(107), in_to_m(42)+keyhole_outer_dia/2])
+
+landing_pad = np.array([in_to_m(70), in_to_m(96), 0])
+large_landing_cube = np.array([in_to_m(48), in_to_m(96), in_to_m(22)])
+small_landing_cube = np.array([in_to_m(30), in_to_m(96), in_to_m(15)])
