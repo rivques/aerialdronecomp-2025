a177d6502ad4eab6e4da4fb5133910b89506928a
rivques <38469076+rivques@users.noreply.github.com>
2025-02-14 13:00:35 -0500

    more tweaks

--- part2.py
+++ part2.py
@@ -17,11 +17,11 @@ if __name__ == "__main__":
         WaitAction(3),
         GoToAction(fl.yellow_keyhole[0]+0.1*X_DRIFT_COEFF, fl.yellow_keyhole[1]-0.2, None, "GoThruYellowKeyhole", 3), # +15 (165 total)
         GoToAction(fl.yellow_keyhole[0]+0.1*X_DRIFT_COEFF, fl.yellow_keyhole[1] + 0.2 + 0.6*X_DRIFT_COEFF, None, "GoBack", 3),
-        GoToAction(0.1, fl.green_keyhole[1]+0.5, None, "GoThruYellowKeyholeAgain", 3), # +15 (180 total)
-        GoToAction(None, None, fl.green_keyhole[2], "LowerForGreenKeyhole", 4),
+        GoToAction(0.1, fl.green_keyhole[1]+0.4, None, "GoThruYellowKeyholeAgain", 3), # +15 (180 total)
+        GoToAction(None, None, fl.green_keyhole[2]-0.05, "LowerForGreenKeyhole", 4),
         GoToAction(fl.green_keyhole[0]-0.2, None, None, "GoThruGreenKeyhole", 3), # +15 (195 total)
-        GoToAction(fl.green_keyhole[0]+0.2 + 0.6*X_DRIFT_COEFF, fl.green_keyhole[1], None, "GoBack", 3),
-        GoToAction(fl.green_keyhole[0]-0.2, fl.green_keyhole[1]+0.3, None, "GoThruGreenKeyholeAgain", 3), # +15 (210 total)
-        GoToAction(fl.large_landing_cube[0]+0.6*X_DRIFT_COEFF, fl.large_landing_cube[1]+1.4*Y_DRIFT_COEFF, None, "GoToLandingCube", 3),
+        GoToAction(fl.green_keyhole[0]+0.2 + 0.6*X_DRIFT_COEFF, None, None, "GoBack", 3),
+        GoToAction(fl.green_keyhole[0]-0.2, None, None, "GoThruGreenKeyholeAgain", 3), # +15 (210 total)
+        GoToAction(fl.large_landing_cube[0]+0.0*X_DRIFT_COEFF, fl.large_landing_cube[1]+1.9*Y_DRIFT_COEFF, None, "GoToLandingCube", 3),
         LandAction() # +25 (235 total)
     ], ErrorHandlingStrategy.LAND).run_sequence()
